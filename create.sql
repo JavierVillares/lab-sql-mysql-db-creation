b8e3efd803ee22f24082d009c51d8447540a6ade
Javivi <161762434+JavierVillares@users.noreply.github.com>
2024-04-29 22:08:23 +0200

    Add files via upload

--- create.sql
+++ create.sql
@@ -1,19 +1,20 @@
 CREATE DATABASE IF NOT EXISTS lab_mysql;
 USE lab_mysql;
+
 DROP TABLE IF EXISTS cars;
 CREATE TABLE cars (
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     vin VARCHAR(30) NOT NULL,
     manufacturer VARCHAR(30) NOT NULL,
     model VARCHAR(20) NOT NULL,
-    year VARCHAR(10),
-    color VARCHAR(20)
+    year YEAR NOT NULL,
+    color VARCHAR(20) NOT NULL
 );
+
 DROP TABLE IF EXISTS customers;
 CREATE TABLE customers (
-	id INT NOT NULL AUTO_INCREMENT,
-    PRIMARY KEY (id),
-    cust_id INT,
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    cust_id INT NOT NULL,
     cust_name VARCHAR(100) NOT NULL,
     cust_phone VARCHAR(20) NOT NULL,
     cust_email VARCHAR(100),
@@ -23,20 +24,21 @@ CREATE TABLE customers (
     cust_country VARCHAR(100) NOT NULL,
     cust_zipcode VARCHAR(20) NOT NULL
 );
+
 DROP TABLE IF EXISTS salespersons;
 CREATE TABLE salespersons (
-    id INT NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     staff_id INT NOT NULL,
     name VARCHAR(100) NOT NULL,
     store VARCHAR(100) NOT NULL,
-    PRIMARY KEY (id)
+    CONSTRAINT UniqueConstraintName UNIQUE (staff_id)
 );
+
 DROP TABLE IF EXISTS invoice;
 CREATE TABLE invoice (
-	id INT NOT NULL AUTO_INCREMENT,
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     invoice_number INT NOT NULL , 
-    date date,
-    PRIMARY KEY (id),
+    date date NOT NULL,
     car INT NOT NULL,
     customer INT NOT NULL,
     salesperson INT NOT NULL,
--- seeding.sql
+++ seeding.sql
@@ -1,12 +1,12 @@
 USE lab_mysql;
-INSERT INTO cars (id, VIN, manufacturer, model, year, color)
+INSERT INTO cars (id, vin, manufacturer, model, year, color)
 VALUES 
-(1, '3K096I98581DHSNUP', 'Volkswagen', 'Tiguan', 2019, 'Blue'),
-(2, 'ZM8G7BEUQZ97IH46V', 'Peugeot', 'Rifter', 2019, 'Red'),
-(3, 'RKXVNNIHLVVZOUB4M', 'Ford', 'Fusion', 2018, 'White'),
-(4, 'HKNDGS7CU31E9Z7JW', 'Toyota', 'RAV4', 2018, 'Silver'),
-(5, 'DAM41UDN3CHU2WVF6', 'Volvo', 'V60', 2019, 'Gray'),
-(6, 'DAM41UDN3CHU2WVF6', 'Volvo', 'V60 Cross Country', 2019, 'Gray');
+(NULL, '3K096I98581DHSNUP', 'Volkswagen', 'Tiguan', 2019, 'Blue'),
+(NULL, 'ZM8G7BEUQZ97IH46V', 'Peugeot', 'Rifter', 2019, 'Red'),
+(NULL, 'RKXVNNIHLVVZOUB4M', 'Ford', 'Fusion', 2018, 'White'),
+(NULL, 'HKNDGS7CU31E9Z7JW', 'Toyota', 'RAV4', 2018, 'Silver'),
+(NULL, 'DAM41UDN3CHU2WVF6', 'Volvo', 'V60', 2019, 'Gray'),
+(NULL, 'DAM41UDN3CHU2WVF6', 'Volvo', 'V60 Cross Country', 2019, 'Gray');
 
 INSERT INTO customers (cust_id, cust_name, cust_phone, cust_email, cust_address, cust_city, cust_state, cust_country, cust_zipcode)
 VALUES
@@ -25,6 +25,10 @@ Values (1,	00001,	'Petey Cruiser','Madrid'),
 (7, 00007, 'Walter Melon', 'Amsterdam' ),
 (8, 00008, 'Shonda Leer', 'São Paulo' );
 
-
+INSERT INTO invoice (id, invoice_number, date, car, customer, salesperson)
+VALUES 
+(1, 852399038, '2018-08-22', 1, 1, 3),
+(2, 731166526, '2018-12-31', 3, 1, 5),
+(3, 271135104, '2019-01-22', 2, 1, 7);
 select * 
 FROM invoice
